2569837ebdaa4c100b8a042d29b34c05f14bf408
Utsav Donda <utsavdonda07@gmail.com>
2022-04-27 23:07:28 +0530

    3nd Commit ui

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:ujphone/reusableCard.dart';
 import 'package:ujphone/reusableCircle.dart';
@@ -19,173 +20,127 @@ class _UJphoneState extends State<UJphone> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
           title: Center(child: Text('Ujphone')),
           actions: [
             IconButton(icon: Icon(Icons.search), onPressed: () {}),
           ],
-          backgroundColor: Color(0xff60e1c8),
+          backgroundColor: Colors.white54,
         ),
-      body: Column(
-        children: [
-          SizedBox(
-            height: 40,
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-            ReusableCircle(),
-            ReusableCircle(),
-            ReusableCircle(),
-          ],),
+      body: SingleChildScrollView(
+        scrollDirection: Axis.vertical,
+        child: Column(
+          // crossAxisAlignment: CrossAxisAlignment.end,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            SizedBox(
+              height: 40,
+            ),
+            SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Row(
+                // crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                ReusableCircle(), SizedBox(
+                    width: 20,
+                  ),
+                ReusableCircle(),SizedBox(
+                    width: 20,
+                  ),
+                ReusableCircle(),SizedBox(
+                    width: 20,
+                  ),
+                  ReusableCircle(),SizedBox(
+                    width: 20,
+                  ),
+                  ReusableCircle(),SizedBox(
+                    width: 20,
+                  ),
+                  ReusableCircle(),
+              ],),
+            ),
 
+            SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
 
-        GridView.builder(
-          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: 2),
-          itemCount:2,
-          padding: EdgeInsets.all(2.0),
-          itemBuilder: (BuildContext context, int index) {
-            return Text(index.toString());
-          },
-
-
-
+                  ReusableCard(cardcolor: Colors.deepOrange,childCard:Text('1'),),
+                  ReusableCard(cardcolor: Colors.deepPurple,childCard:Text('2'),),
+                  ReusableCard(cardcolor: Colors.blue,childCard:Text('3'),),
+                ],),
+            ),
+            SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  ReusableCard(cardcolor: Colors.deepOrange,childCard:Text('1'),),
+                  ReusableCard(cardcolor: Colors.deepPurple,childCard:Text('2'),),
+                  ReusableCard(cardcolor: Colors.blue,childCard:Text('3'),),
+                ],
+              ),
+            ),
+            SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  ReusableCard(cardcolor: Colors.deepOrange,childCard:Text('1'),),
+                  ReusableCard(cardcolor: Colors.deepPurple,childCard:Text('2'),),
+                  ReusableCard(cardcolor: Colors.blue,childCard:Text('3'),),
+                ],
+              ),
+            ),
+            // Expanded(
+            //   child: Row(
+            //     children: [
+            //       Expanded(
+            //         child: Row(
+            //
+            //           children: [
+            //             Expanded(
+            //               child: ReusableCard(
+            //                 cardcolor: ActiveCardColor,
+            //                 childCard: Column(
+            //                   mainAxisAlignment: MainAxisAlignment.center,
+            //                   children: [
+            //                     Text(
+            //                       'HEIGHT',
+            //                       style: TextStyle(fontSize: 20),
+            //                     ),
+            //
+            //                     Row(
+            //                       mainAxisAlignment: MainAxisAlignment.center,
+            //
+            //
+            //                       children: [
+            //                         Text(
+            //                           'hello',
+            //                           style: TextStyle(
+            //                               fontSize: 40, fontWeight: FontWeight.w800),
+            //                         ),
+            //                         Text('100', style: TextStyle(
+            //                           fontSize: 20,),),
+            //                       ],
+            //                     ),
+            //
+            //                   ],
+            //                 ),
+            //               ),
+            //             ),
+            //           ],
+            //         ),
+            //       ),
+            //     ],
+            //   ),
+            // ),
+          ],
         ),
-
-
-
-          Expanded(
-            child: Row(
-              children: [
-                Expanded(
-                  child: Row(
-
-                    children: [
-                      Expanded(
-                        child: ReusableCard(
-                          cardcolor: ActiveCardColor,
-                          childCard: Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              Text(
-                                'HEIGHT',
-                                style: TextStyle(fontSize: 20),
-                              ),
-
-                              Row(
-                                mainAxisAlignment: MainAxisAlignment.center,
-
-
-                                children: [
-                                  Text(
-                                    'hello',
-                                    style: TextStyle(
-                                        fontSize: 40, fontWeight: FontWeight.w800),
-                                  ),
-                                  Text('100', style: TextStyle(
-                                    fontSize: 20,),),
-                                ],
-                              ),
-
-                            ],
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ],
-            ),
-          ),
-          Expanded(
-            child: Row(
-              children: [
-                Expanded(
-                  child: Row(
-
-                    children: [
-                      Expanded(
-                        child: ReusableCard(
-                          cardcolor: ActiveCardColor,
-                          childCard: Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              Text(
-                                'HEIGHT',
-                                style: TextStyle(fontSize: 20),
-                              ),
-
-                              Row(
-                                mainAxisAlignment: MainAxisAlignment.center,
-
-
-                                children: [
-                                  Text(
-                                    'hello',
-                                    style: TextStyle(
-                                        fontSize: 40, fontWeight: FontWeight.w800),
-                                  ),
-                                  Text('100', style: TextStyle(
-                                    fontSize: 20,),),
-                                ],
-                              ),
-
-                            ],
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ],
-            ),
-          ),
-          Expanded(
-            child: Row(
-              children: [
-                Expanded(
-                  child: Row(
-
-                    children: [
-                      Expanded(
-                        child: ReusableCard(
-                          cardcolor: ActiveCardColor,
-                          childCard: Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              Text(
-                                'HEIGHT',
-                                style: TextStyle(fontSize: 20),
-                              ),
-
-                              Row(
-                                mainAxisAlignment: MainAxisAlignment.center,
-
-
-                                children: [
-                                  Text(
-                                    'hello',
-                                    style: TextStyle(
-                                        fontSize: 40, fontWeight: FontWeight.w800),
-                                  ),
-                                  Text('100', style: TextStyle(
-                                    fontSize: 20,),),
-                                ],
-                              ),
-
-                            ],
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ],
       ),
       ),
     );
